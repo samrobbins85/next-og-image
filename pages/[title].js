332607a5e91170795cd782f0b77804c199b4a6bb
Sam Robbins <samrobbinsgb@gmail.com>
2020-12-04 19:43:43 +0000

    Return a custom title

---
+++ pages/[title].js
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+export default function Home() {
+  const router = useRouter();
+  const { title } = router.query;
+
+  return (
+    <div className="flex h-screen justify-center align-center">
+      <h1 className="text-center text-6xl font-semibold self-center">
+        {title}
+      </h1>
+    </div>
+  );
+}
--- pages/api/[title].js
+++ pages/api/[title].js
@@ -1,10 +1,16 @@
 import path from "path";
 const puppeteer = require("puppeteer");
+import absoluteUrl from "next-absolute-url";
 
 export default async function (req, res) {
+  const {
+    query: { title },
+  } = req;
+  const { origin } = absoluteUrl(req, "localhost:3000");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://google.com");
+  await page.goto(origin + "/" + title);
+  await page.setViewport({ width: 1200, height: 627 });
   const imageBuffer = await page.screenshot();
 
   await browser.close();
