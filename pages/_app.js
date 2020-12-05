a52a0cf65bf45254e520f356d2682ff9c459a172
Sam Robbins <samrobbinsgb@gmail.com>
2020-12-05 12:58:24 +0000

    Basic preview

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "tailwindcss/tailwind.css";
+import "../styles/tailwind.css";
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
 export default function Home() {
+  const [title, setTitle] = useState("Set a title");
   return (
-    <div className="flex h-screen justify-center align-center">
-      <h1 className="text-center text-6xl font-semibold self-center">Hello</h1>
-    </div>
+    <>
+      <div>
+        <h1 className="text-center text-4xl font-semibold pt-6">
+          Next.js open graph image as a service
+        </h1>
+      </div>
+      <div className="aspect border mx-auto mt-12 grid">
+        <div className="place-self-center justify-self-center text-center font-semibold text-6xl">
+          {title}
+        </div>
+      </div>
+      <div className="flex justify-center pt-4">
+        <label>
+          <span className="pr-2 text-lg">Title:</span>
+          <input
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </label>
+      </div>
+    </>
   );
 }
